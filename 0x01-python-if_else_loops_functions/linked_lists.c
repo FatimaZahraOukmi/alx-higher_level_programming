dc36c7c9ec5a7c178811524147fdfb48fb8f66d6
FatimaZahraOukmi <Fatimazahra.oukmi@gmail.com>
2023-06-06 09:04:48 -0700

    up

--- 0x01-python-if_else_loops_functions/linked_lists.c
+++ 0x01-python-if_else_loops_functions/linked_lists.c
@@ -28,11 +28,11 @@ size_t print_listint(const listint_t *h)
 
 /**
  * add_nodeint_end - adds a new node at the end of a listint_t list
- * 
+ *
  * @head: pointer to pointer of first node of listint_t list
- * 
+ *
  * @n: integer to be included in new node
- * 
+ *
  * Return: address of the new element or NULL if it fails
  *
  */
